998cb221693228fa772f9f298a7975890b903724
unknown <jhossymarbalderrama@gmail.com>
2023-04-21 13:00:12 -0300

    Creacion de Clases, Componentes necesarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PortafolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortafolioComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
