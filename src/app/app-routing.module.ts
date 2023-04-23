10ed4b3ae9ea2ef3dd0a7fe52e866910cc9db48a
unknown <jhossymarbalderrama@gmail.com>
2023-04-23 14:25:27 -0300

    Se agregaron los paths de Login y Portafolio, y mas arreglos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
 import { PortafolioComponent } from './componentes/portafolio/portafolio.component';
 
 const routes: Routes = [
-  {path: '', component: PortafolioComponent}
+  {path: '', component: PortafolioComponent},
+  {path: 'portafolio', component: PortafolioComponent},
+  {path: 'login', component: LoginComponent},
+  {path:'**', component: LoginComponent}
 ];
 
 @NgModule({
