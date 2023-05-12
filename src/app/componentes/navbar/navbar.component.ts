3e48bf69a6640ae8e00d4bf24b8126a8898f1041
unknown <jhossymarbalderrama@gmail.com>
2023-05-12 11:10:11 -0300

    estilos y CRUD de secciones

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faFlag } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  faFlag:any= faFlag;
   constructor() { }
 
   ngOnInit(): void {
